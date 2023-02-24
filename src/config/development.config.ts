4bcda09feadad2e9b98b1108206b972ae5e67b3a
IronicPickle <nathanrath@live.co.uk>
2023-02-24 01:53:44 +0000

    added badge image picker

--- src/config/development.config.ts
+++ src/config/development.config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./config";
 
 const config: Config = {
-  apiUrl: "http://192.168.68.50:8082",
-  zmqUrl: "tcp://192.168.68.50:3000",
+  apiUrl: "http://192.168.68.52:8082",
+  zmqUrl: "tcp://192.168.68.52:3000",
   httpPort: 8080,
 };
 
