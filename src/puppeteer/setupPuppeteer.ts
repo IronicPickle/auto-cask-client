6d6defcb161df6cf87a75e0f0b581153bd60fa2c
IronicPickle <nathanrath@live.co.uk>
2023-02-26 02:20:25 +0000

    added protocol to puppeteer path

--- src/puppeteer/setupPuppeteer.ts
+++ src/puppeteer/setupPuppeteer.ts
@@ -33,5 +33,5 @@ export default async () => {
 
   const indexPath = path.resolve(path.join(__dirname, "../../build/static/index.html"));
 
-  await page.goto(indexPath);
+  await page.goto(`file://${indexPath}`);
 };
